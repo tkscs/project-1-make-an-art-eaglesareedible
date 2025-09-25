b685e9b0c4649ad4b12f8702b1ac2e79f29243a8
eaglesareedible <zadler2029@kehillahstudent.org>
2025-09-25 09:01:56 -0700

    Hi

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
-import turtle
+from turtle import *
 
-turtle.forward(100)
+def drawEye():
+    begin_fill()
+    circle(30)
+    end_fill()
+drawEye()
+penup()
+forward(300)
+pendown()
+drawEye()
+penup()
+setposition(425, 200)
+pendown()
+left(90)
+circle(250, 180)
+
+for i in range(4):
+    forward(10)
+    right(90/4)
+
+exitonclick()
